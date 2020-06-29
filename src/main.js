961854c095d3e4cc6cba4b562535cad08b68714a
Taylor Somers <tsomers@yandex.com>
2020-06-29 18:25:55 -0400

    Add harvestCrop UI function
    Co-authored-by: Beverly Potts <pottsbeverly@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,7 +2,20 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+import { Farmer } from '../src/farmer.js';
+import { Tomato } from '../src/tomato.js';
+
+function harvestCrop(farmer,crop) {
+  farmer.addPoint();
+  crop.resetHarvestStatus();
+}
+
+const eustace = new Farmer("Eustace");
+const tomato = new Tomato();
 
 $(document).ready(function() {
-  
+  $("button#harvest").click(() => {
+    harvestCrop(eustace,tomato);
+    alert(tomato.harvestStatus);
+  });
 });
