6ba6d3057023f7fe172bb79ba15a05993a91520c
Taylor Somers <tsomers@yandex.com>
2020-06-29 19:03:38 -0400

    Add logic for addCrop prototype method and pass test
    Co-authored-by: Beverly Potts <pottsbeverly@gmail.com>

--- src/farmer.js
+++ src/farmer.js
@@ -2,6 +2,11 @@ export class Farmer {
   constructor(name){
     this.name = name;
     this.points = 0;
+    this.cropArray = [];
+  }
+
+  addCrop(crop) {
+    this.cropArray.push(crop);
   }
 
   addPoint() {
