a1878793410cb5e480f632ea9dc0dd4a2d7405ed
Taylor Somers <tsomers@yandex.com>
2020-06-29 14:19:34 -0400

    Add failing test for waterLevel prototype method
    Co-authored-by: Beverly Potts <pottsbeverly@gmail.com>

--- __tests__/tomato.test.js
+++ __tests__/tomato.test.js
@@ -15,4 +15,8 @@ describe('Tomato', () => {
   test('should create a tomato object with a water level of 30', () => {
     expect(tomato.waterLevel).toEqual(30);
   });
+  test('should decrement tomato water level by 1 every second', () =>{
+    jest.advanceTimersByTime(1001);
+    expect(tomato.waterLevel).toEqual(29);
+  });
 });
--- src/tomato.js
+++ src/tomato.js
@@ -2,4 +2,10 @@ export class Tomato {
   constructor(){
     this.waterLevel = 30;
   }
+
+  setWaterLevel() {
+    // setInterval(() => {
+    //   this.waterLevel--;
+    // }, 1000);
+  }
 }
